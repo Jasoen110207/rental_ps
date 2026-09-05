7dc70f8e1e678f03d7811a13cfdb8ab019a4e4e4
mars22144 <fauziadnansptr@gmail.com>
2026-09-05 11:57:57 +0700

    feat: initialize core database migrations, models, factories, and seeders for application entities

---
+++ app/Models/PlaySession.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PlaySession extends Model
+{
+    /** @use HasFactory<\Database\Factories\PlaySessionFactory> */
+    use HasFactory;
+}
