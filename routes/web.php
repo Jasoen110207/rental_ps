a622236b38728c121e9591b7844b9dface1ea67b
mars22144 <fauziadnansptr@gmail.com>
2026-09-05 13:01:27 +0700

    feat: add test dashboard route and view to display TV and product inventory data

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Models\Tv;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-tampilan', function () {
+    $tvs = Tv::all();
+    $products = Product::all();
+    
+    return view('test-tampilan', compact('tvs', 'products'));
 });
